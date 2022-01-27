f5ac0574f1d2fde761ee606a906ddda215c533ee
enricopic85 <enricodev85@gmail.com>
2022-01-27 14:51:15 +0100

    navbar

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,28 @@
 <template>
   <div id="app">
-   
+    <navbar-app/>
+    <jumbotron-app/>  
+    <main-app/>  
+    <footer-app/>
   </div>
 </template>
 
 <script>
-
-
+import NavbarApp from './components/NavbarApp.vue'
+import JumbotronApp from './components/JumbotronApp.vue'
+import MainApp from './components/MainApp.vue'
+import FooterApp from './components/FooterApp.vue'
 export default {
   name: 'App',
   components: {
-   
+    NavbarApp,
+    JumbotronApp,
+    MainApp,
+    FooterApp
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  
-}
+  @import './style/main.scss'
 </style>
---
+++ src/components/NavbarApp.vue
@@ -0,0 +1,54 @@
+<template>
+    <nav>
+        <img src="../assets/dc-logo.png" alt="">
+        <ul>
+            <li>CHARACTERS</li>
+            <li>COMICS</li>
+            <li>MOVIES</li>
+            <li>TV</li>
+            <li>GAMES</li>
+            <li>COLLECTIBLES</li>
+            <li>VIDEOS</li>
+            <li>FANS</li>
+            <li>NEWS</li>
+            <li>SHOP</li>
+        </ul>
+    </nav>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+
+    nav{
+        background-color: white;
+        width: 100%;
+        height: 100px;
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        img{
+            width: 60px;
+            height: 60px;
+        }
+        ul{
+            display: flex;
+            gap: 20px;
+            height: 100px;
+            line-height: 100px;
+            li{
+                list-style: none;
+                height: 100px;
+                font-family: 'Montserrat', sans-serif;
+                &:hover{
+                     border-bottom: 5px solid blue;
+                     color: blue;
+                }
+            }
+        }
+    }
+</style>
